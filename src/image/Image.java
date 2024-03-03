a68bee8a51131a52312166ff8238d7e32089c3da
tamarwi <widmann.tamar@gmail.com>
2024-03-03 13:38:40 +0200

    without debug

--- src/image/Image.java
+++ src/image/Image.java
@@ -7,8 +7,9 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * A package-private class of the package image.
+ * A package-private class of the package image that represents an image as a 2D Color array.
  *
+ * @see Color
  * @author Dan Nirel
  */
 public class Image {
